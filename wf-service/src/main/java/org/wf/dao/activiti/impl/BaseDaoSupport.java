f99a80953bab1177af995df3a3497a4523a5956d
runjia <runjiamail@gmail.com>
2014-10-08 12:51:33 +0800

    InitDestroyAnnotationBeanPostProcessor (Ordered.LOWEST_PRECEDENCE) vs. CommonAnnotationBeanPostProcessor (Ordered.LOWEST_PRECEDENCE - 3)

--- wf-service/src/main/java/org/wf/dao/activiti/impl/BaseDaoSupport.java
+++ wf-service/src/main/java/org/wf/dao/activiti/impl/BaseDaoSupport.java
@@ -3,6 +3,7 @@ package org.wf.dao.activiti.impl;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import org.springframework.core.Ordered;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
@@ -16,7 +17,10 @@ public class BaseDaoSupport extends SqlMapClientDaoSupport {
 	private SqlMapClient sqlMapClient;
 	
 	/**
-	 * @PostConstruct has lower priority compared with JSR-250 @Resource
+	 * JSR-250 @PostConstruct has a lower priority than JSR-250 @Resource,
+	 * <br>
+	 * InitDestroyAnnotationBeanPostProcessor (Ordered.LOWEST_PRECEDENCE) vs.
+	 *  CommonAnnotationBeanPostProcessor (Ordered.LOWEST_PRECEDENCE - 3)
 	 */
 	@PostConstruct
 	public void setSuper(){
